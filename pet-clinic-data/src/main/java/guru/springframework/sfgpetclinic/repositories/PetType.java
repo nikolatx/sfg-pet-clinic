20ec56769e9fe7c5acf420c4900ea36b87272b08
Nikola Timotijevic <nikolatx@gmail.com>
2021-01-20 18:37:20 +0100

    Adding Spring Data JPA Service for Owners. Closes #40

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetType.java
+++
@@ -1,6 +0,0 @@
-package guru.springframework.sfgpetclinic.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PetType extends CrudRepository<PetType, Long> {
-}
