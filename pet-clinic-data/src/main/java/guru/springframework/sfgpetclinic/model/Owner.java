7ae2be3a8d17d30b525c395443b3b5ed01542ae2
Nikola Timotijevic <nikolatx@gmail.com>
2021-08-19 20:04:08 +0200

    Adding Visit entity, updating Owner with pet. Closes #26

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,7 +1,17 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Owner extends Person {
 
+    private Set<Pet> pets=new HashSet<>();
 
+    public Set<Pet> getPets() {
+        return pets;
+    }
 
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
