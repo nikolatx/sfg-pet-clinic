d446efa3c4208a8891f5b597593a3f940577eb49
Nikola Timotijevic <nikolatx@gmail.com>
2021-10-21 22:33:10 +0200

    Adding ability to create new owner and update existing owner, closes #55

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -6,12 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 
@@ -57,10 +55,37 @@ public class OwnerController {
     }
 
     @GetMapping("/{ownerId}")
-    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+    public ModelAndView showOwner(@PathVariable Long ownerId) {
         ModelAndView mav = new ModelAndView("owners/ownerDetails");
         mav.addObject(ownerService.findById(ownerId));
         return mav;
+    }
+
+    @GetMapping("/new")
+    public String showNewOwnerForm(Model model) {
+        model.addAttribute("owner", new Owner());
+        return "owners/createOrUpdateOwnerForm";
+    }
+
+    @PostMapping("/new")
+    @Transactional
+    public String processNewOwnerForm(@ModelAttribute Owner owner) {
+        Owner savedOwner = ownerService.save(owner);
+        return "redirect:/owners/" + savedOwner.getId();
+    }
+
+    @GetMapping("/{id}/edit")
+    public String initUpdateForm(@PathVariable String id, Model model) {
+        model.addAttribute("owner", ownerService.findById(Long.valueOf(id)));
+        return "owners/createOrUpdateOwnerForm";
+    }
+
+    @PostMapping("/{id}/edit")
+    @Transactional
+    public String processUpdateForm(@PathVariable String id, @ModelAttribute Owner owner) {
+        owner.setId(Long.valueOf(id));
+        Owner savedOwner = ownerService.save(owner);
+        return "redirect:/owners/" + savedOwner.getId();
     }
 
 }
