db4444a4e71912373c04f79d70b501014a108428
Nikola Timotijevic <nikolatx@gmail.com>
2021-10-21 18:57:10 +0200

    Fixing find owner by like, closes #64

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -41,7 +41,7 @@ public class OwnerController {
         if (owner.getLastName()==null)
             owner.setLastName("");
 
-        List<Owner> results = ownerService.findAllByLastNameLike(owner.getLastName());
+        List<Owner> results = ownerService.findAllByLastNameLike("%" + owner.getLastName() + "%");
 
         if (results.isEmpty()) {
             result.rejectValue("lastName", "notFound", "not found");
